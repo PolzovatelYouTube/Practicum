18be94c2c966ebea3f5df908a3469e3a11b7c185
PolzovatelYouTube <91389970+PolzovatelYouTube@users.noreply.github.com>
2024-09-28 10:50:07 +0300

    Add files via upload

--- CrazyFrog.py
+++ CrazyFrog.py
@@ -6,28 +6,19 @@ from PyQt6.QtGui import *
 
 class Frog(QGraphicsEllipseItem):
     def __init__(self, x, y, weight, max_jump_distance):
-        super().__init__(0, 0, 20, 20)  # размеры лягушки
+        super().__init__(-10, -10, 20, 20)  # размеры лягушки
         self.setPos(QPointF(x, y))  # Начальная позиция лягушки
         self.weight = weight
         self.max_jump_distance = max_jump_distance
         self.setBrush(QBrush(QColor("red")))
         self.setZValue(10)
-    '''
-    def getPos(self):
-        return QGraphicsEllipseItem.pos(self)
-
-    def setPos(self, pos):
-        QGraphicsEllipseItem.setPos(self, pos)
-    '''
 
     def crazy_jump(self, where):
         self.setPos(where.x(), where.y())  # Устанавливаем как x, так и y
 
-    #pos = pyqtProperty(QPointF, getPos, setPos)
-
 class LilyPad(QGraphicsEllipseItem):
     def __init__(self, x, y, strength):
-        super().__init__(0, 0, 30, 30)  # размеры кувшинки
+        super().__init__(-15, -15, 30, 30)  # размеры кувшинки
         self.setPos(x, y)  # Устанавливаем координаты (x, y)
         self.strength = strength
         self.setBrush(QBrush(QColor("green")))
@@ -43,19 +34,99 @@ class LilyPad(QGraphicsEllipseItem):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setFixedSize(800, 600)
-        self.scene = QGraphicsScene(0, 0, 800, 600) 
+        self.setFixedSize(1000, 600)  # Установка фиксированного размера окна 1000x600
+        self.scene = QGraphicsScene(0, 0, 800, 600)  # Создание сцены 800x600
         self.view = QGraphicsView(self.scene, self)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.setCentralWidget(self.view)
-        
-        #след
+        self.view.setGeometry(200, 0, 800, 600)  # Установка геометрии QGraphicsView
+
+        # Переменные для управления
+        self.trail_length = 2
+        self.fall_speed = 2
+        self.spawn_interval = 500
+        self.lily_weight_max = 5
+        self.jump_update_interval = 100
+        self.lily_pad_fall_update_interval = 20
+        self.max_lilies = 7
+        self.frog_weight = 5
+        self.frog_jump_distance = 200
+
+        # Создание левой полоски для настроек параметров
+        self.settings_panel = QWidget(self)
+        self.settings_panel.setGeometry(0, 0, 200, 600)
+        self.settings_layout = QVBoxLayout(self.settings_panel)
+
+        # Добавление элементов настроек параметров в левую полоску
+        self.fall_speed_slider = QSlider(Qt.Orientation.Horizontal)
+        self.fall_speed_slider.setRange(1, 8)
+        self.fall_speed_slider.setValue(self.fall_speed)
+        self.fall_speed_slider.valueChanged.connect(self.update_fall_speed)
+        self.settings_layout.addWidget(QLabel("Скорость падения"))
+        self.settings_layout.addWidget(self.fall_speed_slider)
+
+        self.spawn_interval_slider = QSlider(Qt.Orientation.Horizontal)
+        self.spawn_interval_slider.setRange(100, 750)
+        self.spawn_interval_slider.setValue(self.spawn_interval)
+        self.spawn_interval_slider.valueChanged.connect(self.update_spawn_interval)
+        self.settings_layout.addWidget(QLabel("Интервал спавна"))
+        self.settings_layout.addWidget(self.spawn_interval_slider)
+
+        self.lily_weight_max_slider = QSlider(Qt.Orientation.Horizontal)
+        self.lily_weight_max_slider.setRange(1, 20)
+        self.lily_weight_max_slider.setValue(self.lily_weight_max)
+        self.lily_weight_max_slider.valueChanged.connect(self.update_lily_weight_max)
+        self.settings_layout.addWidget(QLabel("Максимальный вес кувшинки"))
+        self.settings_layout.addWidget(self.lily_weight_max_slider)
+
+        self.jump_update_interval_slider = QSlider(Qt.Orientation.Horizontal)
+        self.jump_update_interval_slider.setRange(2, 1000)
+        self.jump_update_interval_slider.setValue(self.jump_update_interval)
+        self.jump_update_interval_slider.valueChanged.connect(self.update_jump_update_interval)
+        self.settings_layout.addWidget(QLabel("Интервал обновления прыжка"))
+        self.settings_layout.addWidget(self.jump_update_interval_slider)
+
+        self.lily_pad_fall_update_interval_slider = QSlider(Qt.Orientation.Horizontal)
+        self.lily_pad_fall_update_interval_slider.setRange(1, 20)
+        self.lily_pad_fall_update_interval_slider.setValue(self.lily_pad_fall_update_interval)
+        self.lily_pad_fall_update_interval_slider.valueChanged.connect(self.update_lily_pad_fall_update_interval)
+        self.settings_layout.addWidget(QLabel("Интервал обновления падения кувшинки"))
+        self.settings_layout.addWidget(self.lily_pad_fall_update_interval_slider)
+
+        self.max_lilies_slider = QSlider(Qt.Orientation.Horizontal)
+        self.max_lilies_slider.setRange(0, 10)
+        self.max_lilies_slider.setValue(self.max_lilies)
+        self.max_lilies_slider.valueChanged.connect(self.update_max_lilies)
+        self.settings_layout.addWidget(QLabel("Максимальное количество кувшинок"))
+        self.settings_layout.addWidget(self.max_lilies_slider)
+
+        self.frog_weight_slider = QSlider(Qt.Orientation.Horizontal)
+        self.frog_weight_slider.setRange(0, 20)
+        self.frog_weight_slider.setValue(self.frog_weight)
+        self.frog_weight_slider.valueChanged.connect(self.update_frog_weight)
+        self.settings_layout.addWidget(QLabel("Вес лягушки"))
+        self.settings_layout.addWidget(self.frog_weight_slider)
+
+        self.frog_jump_distance_slider = QSlider(Qt.Orientation.Horizontal)
+        self.frog_jump_distance_slider.setRange(100, 500)
+        self.frog_jump_distance_slider.setValue(self.frog_jump_distance)
+        self.frog_jump_distance_slider.valueChanged.connect(self.update_frog_jump_distance)
+        self.settings_layout.addWidget(QLabel("Максимальное расстояние прыжка лягушки"))
+        self.settings_layout.addWidget(self.frog_jump_distance_slider)
+
+        self.trail_length_slider = QSlider(Qt.Orientation.Horizontal)
+        self.trail_length_slider.setRange(0, 20)
+        self.trail_length_slider.setValue(self.trail_length)
+        self.trail_length_slider.valueChanged.connect(self.update_trail_length)
+        self.settings_layout.addWidget(QLabel("Длина следа"))
+        self.settings_layout.addWidget(self.trail_length_slider)
+
+        # Cлед
         self.lines = []
 
         # Берега
         self.left_shore = 0
-        self.right_shore = self.width() - 50
+        self.right_shore = self.scene.width()
         self.current_shore = 'left'
 
         # Логика движения
@@ -64,62 +135,88 @@ class MainWindow(QMainWindow):
         # Создание списка кувшинок
         self.lilies = []
 
-        # Создание лягушки
-        self.frog = Frog(0, 300, weight=5, max_jump_distance=300)
+        # Текущая кувшинка
+        self.previous_lily_pad = None
+
+        # Лягушка
+        self.frog = Frog(400, 300, weight=self.frog_weight, max_jump_distance=self.frog_jump_distance)
         self.scene.addItem(self.frog)
 
-        # Таймер для прыжков лягушки
+        # Таймеры
         self.timer_frog = QTimer()
         self.timer_frog.timeout.connect(self.update_frog_position)
-        self.timer_frog.start(100)  # Обновление каждые 0.1 секунды
+        self.timer_frog.start(self.jump_update_interval)
 
-        # Таймер для падения кувшинок
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_lily_pads_position)
-        self.timer.start(20)  # Обновление каждые n мс для плавного падения
+        self.timer.start(self.lily_pad_fall_update_interval)
 
-        # Таймер для добавления новых кувшинок с задержкой
         self.spawn_timer = QTimer()
         self.spawn_timer.timeout.connect(self.spawn_lily_pads)
-        self.spawn_timer.start()
+        self.spawn_timer.start(0)
+
+    #апдейты
+    def update_trail_length(self, value):
+        self.trail_length = value
+    
+    def update_fall_speed(self, value):
+        self.fall_speed = value
+
+    def update_spawn_interval(self, value):
+        self.spawn_interval = value
+
+    def update_lily_weight_max(self, value):
+        self.lily_weight_max = value
+
+    def update_jump_update_interval(self, value):
+        self.jump_update_interval = value
+        self.timer_frog.start(self.jump_update_interval)
+
+    def update_lily_pad_fall_update_interval(self, value):
+        self.lily_pad_fall_update_interval = value
+        self.timer.start(self.lily_pad_fall_update_interval)
+
+    def update_max_lilies(self, value):
+        self.max_lilies = value
+
+    def update_frog_weight(self, value):
+        self.frog.weight = value
+
+    def update_frog_jump_distance(self, value):
+        self.frog_jump_distance = value
+        self.frog.max_jump_distance = value
+
+    def update_lily_pads_position(self):
+        for lily in self.lilies:
+            lily.fall(self.fall_speed)
+            if lily.pos().y() > self.scene.height():
+                self.scene.removeItem(lily)
+                self.lilies.remove(lily)
 
     def spawn_lily_pads(self):
-        self.spawn_timer.start(random.randint(500, 1000))
-        # Случайное количество кувшинок от 0 до n
-        number_of_lilies = random.randint(1, 7)
+        self.spawn_timer.start(random.randint(self.spawn_interval-100, self.spawn_interval+100))
+        # Случайное количество кувшинок от 1 до n
+        number_of_lilies = random.randint(1, self.max_lilies)
         positions = []  # Список для хранения позиций, чтобы избежать пересечения
 
         for _ in range(number_of_lilies):
             # Случайное положение по оси X, с проверкой на расстояние между кувшинками
             while True:
-                x = random.randint(0, self.scene.width() - 30)  # 30 - ширина кувшинки
+                x = random.randint(self.frog_jump_distance // 4, self.scene.width() - self.frog_jump_distance // 4)  
                 if all(abs(x - pos_x) > 40 for pos_x in positions):  # 40 пикселей - минимальное расстояние
                     positions.append(x)
                     break
             
             y = 0  # Начальное положение по оси Y (сверху)
             # Добавляем кувшинку в сцену с позицией (x, y)
-            strength = random.randint(5, 5)  # Случайная прочность
+            strength = random.randint(1, self.lily_weight_max)  # Случайная прочность
             lily = LilyPad(x, y, strength) 
             self.scene.addItem(lily)
             self.lilies.append(lily)
 
-    def update_lily_pads_position(self):
-        # Определяем скорость падения
-        fall_speed = 2  # Скорость падения кувшинок
-
-        for lily in self.lilies:
-            # Падаем с заданной скоростью
-            lily.fall(fall_speed)
-
-             # Если кувшинка вышла за пределы окна, удаляем её
-            if lily.pos().y() > self.height():
-                self.scene.removeItem(lily)
-                self.lilies.remove(lily)  # Удаляем из списка кувшинок
-
     def find_next_lily_pad(self):
         possible_lilies = []
-        current_pos = self.frog.scenePos()
+        current_pos = self.frog.pos()
 
         # Определяем расстояние до левого и правого берегов
         distance_to_left_shore = abs(current_pos.x() - self.left_shore)
@@ -134,29 +231,28 @@ class MainWindow(QMainWindow):
 
         # Обычный поиск кувшинок
         for lily in self.lilies:
-            distance_x = abs(lily.scenePos().x() - current_pos.x())
-            distance_y = abs(lily.scenePos().y() - current_pos.y())
+            distance_x = abs(lily.pos().x() - current_pos.x())
+            distance_y = abs(lily.pos().y() - current_pos.y())
             distance = (distance_x ** 2 + distance_y ** 2) ** 0.5
 
-            if distance <= self.frog.max_jump_distance and self.frog.weight <= lily.strength and distance >= 2:
+            if distance <= self.frog.max_jump_distance and distance > 20:
                 possible_lilies.append(lily)
 
         if possible_lilies:
             if self.direction == 'right':
-                next_lily = min(possible_lilies, key=lambda l: l.scenePos().x())  # Ближайшая вправо
+                next_lily = max(possible_lilies, key=lambda l: l.pos().x())  # Ближайшая вправо
             else:
-                next_lily = max(possible_lilies, key=lambda l: l.scenePos().x())  # Ближайшая влево
+                next_lily = min(possible_lilies, key=lambda l: l.pos().x())  # Ближайшая влево
 
-            return next_lily.scenePos()
+            return next_lily.pos()
         
         return None
 
 
     def update_frog_position(self):
         next_position = self.find_next_lily_pad()
-        
         if next_position:
-            # Найти кувшинку, на которую прыгнула лягушка
+            # Найти кувшинку, c которой прыгнула лягушка
             jumped_lily = None
             for lily in self.lilies:
                 if lily.pos() == next_position:
@@ -167,20 +263,25 @@ class MainWindow(QMainWindow):
             self.draw_path(self.frog.pos(), next_position)
             self.frog.crazy_jump(next_position)
 
-            # Уменьшение прочности кувшинки после прыжка
-            if jumped_lily:
-                jumped_lily.strength -= self.frog.weight  # Снимаем прочность на вес лягушки
-                if jumped_lily.strength <= 0:
-                    # Если прочность кувшинки достигла нуля, удаляем её из сцены
-                    self.scene.removeItem(jumped_lily)
-                    self.lilies.remove(jumped_lily)
+            # Если лягушка прыгнула с предыдущей кувшинки, снижаем её прочность
+            if self.previous_lily_pad:
+                if self.previous_lily_pad in self.lilies:  # Проверяем, что кувшинка всё ещё в списке
+                    self.previous_lily_pad.strength -= self.frog.weight  # Уменьшаем прочность на вес лягушки
+                    if self.previous_lily_pad.strength <= 0:
+                        # Если прочность кувшинки достигла нуля, удаляем её из сцены
+                        self.scene.removeItem(self.previous_lily_pad)
+                        self.lilies.remove(self.previous_lily_pad)
+
+            self.previous_lily_pad = jumped_lily
+
         else:
             # Если нет доступных кувшинок для прыжка, лягушка следует за кувшинкой вниз
-            current_pos = self.frog.scenePos()
+            current_pos = self.frog.pos()
+            self.draw_path(self.frog.pos(), current_pos)
             for lily in self.lilies:
-                if abs(lily.scenePos().x() - current_pos.x()) < 1:  # Если лягушка находится на кувшинке
+                if abs(lily.pos().x() - current_pos.x()) < 1:  # Если лягушка находится на кувшинке
                     # Следуем за кувшинкой вниз
-                    self.frog.setPos(lily.scenePos().x(), lily.scenePos().y())
+                    self.frog.setPos(lily.pos().x(), lily.pos().y())
 
 
         # Проверка, достигла ли лягушка берега, чтобы сменить направление
@@ -191,13 +292,14 @@ class MainWindow(QMainWindow):
         
     def draw_path(self, start_pos, end_pos):
         pen = QPen(QColor("blue"))
-        pen.setWidth(2)
-        line = self.scene.addLine(start_pos.x()+10, start_pos.y()+10, end_pos.x()+10, end_pos.y()+10, pen)
+        pen.setWidth(1)
+        line = self.scene.addLine(start_pos.x(), start_pos.y(), end_pos.x(), end_pos.y(), pen)
         self.lines.append(line)
 
-        if len(self.lines) > 2:
-            old_line = self.lines.pop(0)
-            self.scene.removeItem(old_line)
+        if len(self.lines) > self.trail_length:
+            while len(self.lines) > self.trail_length:
+                old_line = self.lines.pop(0)
+                self.scene.removeItem(old_line)
 
 
 if __name__ == '__main__':
